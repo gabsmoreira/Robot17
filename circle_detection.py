f3422999f1a464343a945504403b7c27e681b11e
gabsmoreira <gabriel.moreira98@hotmail.com>
2017-02-16 12:21:09 -0200

    file modification

--- circle_detection.py
+++ circle_detection.py
@@ -6,11 +6,11 @@ import cv2
 cap = cv2.VideoCapture(0)
 h = 6.5*37.795672
 f = 17.75
-
+font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
 	ret, img = cap.read()
-	image =cv2.GaussianBlur(img,(5,5),0)
-	output = image.copy()
+	image =cv2.GaussianBlur(img,(5,5),10)
+	#output = image.copy()
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
@@ -23,7 +23,8 @@ while True:
 		for (x, y, r) in circles:
 			cv2.circle(img, (x, y), r, (0, 255, 0), 4)
 			hpix = 2*r
-			print("Distância em cm:",((f*h/hpix))) 
+			print("Distancia em cm:",((f*h/hpix)))
+		cv2.putText(img,str((f*h/hpix)),(200,100), font, 2,(255,255,255),2) 
 
 
 	cv2.imshow("output", img)
